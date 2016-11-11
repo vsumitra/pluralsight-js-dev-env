4df8273c2067b0f13e0d6c5daca3741570e47fdb
Vala Sumitra <vsumitra@gmail.com>
2016-11-11 14:48:35 -0800

    Editor-PackageManager-DevWebServer-Automation-Transpiling-Bundling

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,26 @@
+import config from '../webpack.config.dev'
+import express from 'express'
+import open from 'open'
+import path from 'path'
+import webpack from 'webpack'
+
+const port = 3000
+const app = express()
+const complier = webpack(config)
+
+app.use(require('webpack-dev-middleware')(complier, {
+  'onInfo': true,
+  'publicPath': config.output.publicPath
+}))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    open(`http://localhost:${port}`)
+  }
+})
